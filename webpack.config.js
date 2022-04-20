1835d9822609b052aa535b31d14468539914e051
nivan2 <naranivan@gmail.com>
2022-04-20 04:47:10 -0400

    finished the skeleton hopefully

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'application.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+  }
+};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
